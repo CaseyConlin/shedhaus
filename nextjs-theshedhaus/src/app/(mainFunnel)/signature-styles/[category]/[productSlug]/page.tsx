a2b5f23847eba1faa15b3d839c9e6ef9e392060c
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2026-08-24 18:08:21 -0400

    Add small local towns message to product page.

--- nextjs-theshedhaus/src/app/(mainFunnel)/signature-styles/[category]/[productSlug]/page.tsx
+++ nextjs-theshedhaus/src/app/(mainFunnel)/signature-styles/[category]/[productSlug]/page.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { Metadata } from "next";
 import { PortableText } from "next-sanity";
 import { PageHeader } from "@/components/text/PageHeader";
@@ -12,6 +13,235 @@ import { generateProductStructuredData } from "@/lib/sanity/structured-data";
 import { ProductStructuredData } from "@/components/ProductStructuredData";
 import { ProductList } from "@/components/productList/ProductList";
 
+const preferredTowns = [
+  "Ardsley",
+  "Hastings",
+  "Mount Pleasant",
+  "Elmsford",
+  "North Castle",
+  "Verplank",
+  "Buchanan",
+  "New Castle",
+];
+
+const otherTowns = [
+  "Accord",
+  "Adams",
+  "Airmont",
+  "Alford",
+  "Amawalk",
+  "Amenia",
+  "Ancram",
+  "Ancramdale",
+  "Armonk",
+  "Austerlitz",
+  "Baldwin Place",
+  "Beacon",
+  "Bedford",
+  "Bedford Corners",
+  "Bedford Hills",
+  "Bedford Village",
+  "Bethel",
+  "Bohemia",
+  "Brewster",
+  "Brewster Hill",
+  "Briarcliff",
+  "Briarcliff Manor",
+  "Bridgehampton",
+  "Bridgewater",
+  "Bronxville",
+  "Brookfield",
+  "Campbell Hall",
+  "Carmel",
+  "Chappaqua",
+  "Chelsea",
+  "Clinton Corners",
+  "Cold Spring",
+  "Congers",
+  "Copake",
+  "Copake Falls",
+  "Copake Lake",
+  "Cornwall",
+  "Cornwall Bridge",
+  "Cortlandt Manor",
+  "Cos Cob",
+  "Cross River",
+  "Croton on Hudson",
+  "Danbury",
+  "Darien",
+  "Dobbs Ferry",
+  "Dover",
+  "Dover Plains",
+  "East FIshkill",
+  "Eastchester",
+  "Elizaville",
+  "Falls Village",
+  "Fishkill",
+  "Fresh Meadows",
+  "Garrison",
+  "Gaylordsville",
+  "Gent",
+  "Glenham",
+  "Goldens Bridge",
+  "Goshen",
+  "Granite Springs",
+  "Great Barrington",
+  "Greenwich",
+  "Harrison",
+  "Hartsdale",
+  "Hawthorne",
+  "Highland",
+  "Hillsdale",
+  "HOLBROOK",
+  "Holmes",
+  "Hopewell Junction",
+  "Hyde Park",
+  "Katonah",
+  "Kent",
+  "Kent Lakes NY",
+  "Kent.",
+  "Kingston",
+  "Lagrange",
+  "Lagrangeville",
+  "Lake Carmel",
+  "Lake Peekskill",
+  "Lakeville",
+  "Larchmont",
+  "Leola",
+  "Lewisboro",
+  "Liberty",
+  "Lincolndale",
+  "Litchfield",
+  "Lloyd Harbor",
+  "Mahopac",
+  "Mamaroneck",
+  "Mill River",
+  "Millbrook",
+  "Millerton",
+  "Modena",
+  "Montrose",
+  "Montvale",
+  "Mount Kisco",
+  "Mount Washington",
+  "Mt. Kisco",
+  "Mt. Washington",
+  "New Britain",
+  "New Canaan",
+  "New City",
+  "New Fairfield",
+  "New Marlboro",
+  "New Marlborough",
+  "New Milford",
+  "New Paltz",
+  "New Preston",
+  "New Rochelle",
+  "New York",
+  "Newburgh",
+  "Newton",
+  "Newtown",
+  "Norwalk",
+  "North Egremont",
+  "North Salem",
+  "Northcreek",
+  "Norwalk",
+  "Ossining",
+  "Palisades",
+  "Patterson",
+  "Pawling",
+  "Peekskill",
+  "Pelham",
+  "Pine Plains",
+  "Plainview",
+  "Pleasant Valley",
+  "Pleasantville",
+  "Port Chester",
+  "Poughkeepsie",
+  "Poughquag",
+  "Pound Ridge",
+  "Purchase",
+  "Purdys",
+  "Putnam Lake",
+  "Putnam Valley",
+  "Red Hook",
+  "Redding",
+  "Rhinebeck",
+  "Richmond",
+  "Ridgefield",
+  "Ridgewood",
+  "Rock Hill",
+  "Roscoe",
+  "Roxbury",
+  "Rye",
+  "Salisbury",
+  "Salt Point",
+  "Sandy Hook",
+  "Santa Fe",
+  "Scarsdale",
+  "Sharon",
+  "Sherman",
+  "Sleepy Hollow",
+  "Somers",
+  "South Kent",
+  "South Salem",
+  "South Windsor",
+  "Southbury",
+  "Southeast",
+  "Southhold",
+  "Spencer",
+  "Stamford",
+  "Stanfordville",
+  "Stony Point",
+  "Stormville",
+  "Tarrytown",
+  "Thornwood",
+  "Torrington",
+  "Tyringham",
+  "Union Vale",
+  "Valhalla",
+  "Verbank",
+  "Waccabuc",
+  "Walden",
+  "Wamtagh",
+  "Wappingers Falls",
+  "Warren",
+  "Washington",
+  "Wassaic",
+  "West Cornwall",
+  "West Harrison",
+  "Westport",
+  "White Plains",
+  "Windsor Mill",
+  "Wingdale",
+  "Woodbury",
+  "Yonkers",
+  "Yorktown",
+  "Yorktown Hgts",
+];
+const getLocalityMessage = (): ReactNode => {
+  const randomPreferredTown1 =
+    preferredTowns[Math.floor(Math.random() * preferredTowns.length)];
+  const newTownList = preferredTowns.filter(
+    (town) => town !== randomPreferredTown1,
+  );
+  const randomPreferredTown2 =
+    newTownList[Math.floor(Math.random() * newTownList.length)];
+  const randomOtherTown =
+    otherTowns[Math.floor(Math.random() * otherTowns.length)];
+
+  return (
+    <p className="text-center  font-medium">
+      We deliver to{" "}
+      <span className="font-bold text-primary">{randomPreferredTown1}</span>,{" "}
+      <span className="font-bold text-primary">{randomPreferredTown2}</span>,
+      and other towns in{" "}
+      <span className="font-bold">
+        Westchester, Putnam, Dutchess, and Fairfield Counties
+      </span>
+      , including{" "}
+      <span className="font-bold text-primary">{randomOtherTown}</span>.
+    </p>
+  );
+};
 interface ProductPageProps {
   params: Promise<{
     category: string;
@@ -173,7 +403,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
       </div>
       {suggestedProducts && suggestedProducts.length > 0 && (
         <div className="w-full my-12">
-          <div className="bg-[#f2f2f2] border-gray-300 border-[0.5px] shadow-xl">
+          <div className="bg-[#f2f2f2] border-gray-300 border-[0.5px] shadow-xl py-5">
             <div className="flex flex-col items-center justify-center w-screen">
               <div className="w-full md:px-4 py-8">
                 <h2 className="text-2xl font-bold text-center font-montserrat">
@@ -182,6 +412,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
                 <div className="flex flex-col md:flex-row items-start justify-center max-w-6xl gap-4 md:gap-10 py-4 md:py-8 md:px-0 mx-auto">
                   <ProductList products={suggestedProducts} />
                 </div>
+                <div>{getLocalityMessage()}</div>
               </div>
             </div>
           </div>
